0a803308d83e7931684aa427e9a3ba55c1cafe6e
shivamuhammad <shiva@venturology.id>
2024-10-17 23:21:15 +0700

    add color assets

--- lib/core/constant/styles/colors.dart
+++ lib/core/constant/styles/colors.dart
@@ -5,8 +5,9 @@ class AppColors {
   static const Color primary = Color(0xff1F1D2B);
   static const Color accentOrange = Color(0xffEB933C);
   static const Color lightWhite = Color(0xffFCFCFC);
-  static const darkGray = Color(0xff5F5F5F);
-  static const mediumGray = Color(0xffCBCBCB);
-  static const darkSlate = Color(0xff2E3338);
+  static const Color darkGray = Color(0xff5F5F5F);
+  static const Color mediumGray = Color(0xffCBCBCB);
+  static const Color darkSlate = Color(0xff2E3338);
   static const Color white = Color(0xffFFFFFF);
+  static const Color lightGray2 = Color(0xffDBDBDB);
 }
