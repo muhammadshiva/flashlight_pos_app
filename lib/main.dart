d0eafbf47015a1027ef81d86a70106b1470830f7
shivamuhammad <shiva@venturology.id>
2024-10-17 11:45:04 +0700

    setup main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'core/constant/styles/colors.dart';
+import 'core/constant/styles/fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,7 +14,32 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+
+    return MaterialApp(
+      title: 'Flashlight Point of Sales App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+        useMaterial3: true,
+        textTheme: GoogleFonts.quicksandTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        appBarTheme: AppBarTheme(
+          color: AppColors.white,
+          elevation: 0,
+          titleTextStyle: GoogleFonts.quicksand(
+            color: AppColors.primary,
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+          iconTheme: const IconThemeData(
+            color: AppColors.primary,
+          ),
+        ),
+      ),
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
