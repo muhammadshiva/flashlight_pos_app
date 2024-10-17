93136f2e43f3826623545a56e6f6be62f4cd7d9c
shivamuhammad <shiva@venturology.id>
2024-10-17 10:07:09 +0700

    add app fonts

---
+++ lib/core/constant/styles/fonts.dart
@@ -0,0 +1,21 @@
+import 'package:flashlight_pos_app/core/constant/styles/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle poppins = GoogleFonts.poppins(
+  color: AppColors.lightWhite,
+  decoration: TextDecoration.none,
+);
+
+TextStyle barlow = GoogleFonts.barlow(
+  color: AppColors.lightWhite,
+  decoration: TextDecoration.none,
+);
+
+const FontWeight light = FontWeight.w300; // Light
+const FontWeight regular = FontWeight.w400; // Regular
+const FontWeight medium = FontWeight.w500; // Medium
+const FontWeight semiBold = FontWeight.w600; // Semibold
+const FontWeight bold = FontWeight.w700; // Bold
+const FontWeight extraBold = FontWeight.w800; // Extrabold
+const FontWeight black = FontWeight.w900; // Black
