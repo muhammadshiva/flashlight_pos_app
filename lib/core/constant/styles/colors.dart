8cfd183ee4a5667cc9cdda1ce4feea9646b8aed8
shivamuhammad <shiva@venturology.id>
2024-10-17 10:06:57 +0700

    add app colors

---
+++ lib/core/constant/styles/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xff1F1D2B);
+  static const Color accentOrange = Color(0xffEB933C);
+  static const Color lightWhite = Color(0xffFCFCFC);
+  static const darkGray = Color(0xff5F5F5F);
+  static const mediumGray = Color(0xffCBCBCB);
+  static const darkSlate = Color(0xff2E3338);
+}
