5b65528c447265b94eeda0d374ac4407d5699ead
shivamuhammad <shiva@venturology.id>
2024-10-18 10:48:49 +0700

    add auth bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flashlight_pos_app/presentation/auth/bloc/login/login_bloc.dart';
+import 'package:flashlight_pos_app/presentation/auth/data/datasources/auth_remote_datasource.dart';
 import 'package:flashlight_pos_app/presentation/splash/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'core/constant/styles/colors.dart';
@@ -19,28 +22,31 @@ class MainApp extends StatelessWidget {
       DeviceOrientation.landscapeLeft,
     ]);
 
-    return MaterialApp(
-      title: 'Flashlight Point of Sales App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
-        useMaterial3: true,
-        textTheme: GoogleFonts.quicksandTextTheme(
-          Theme.of(context).textTheme,
-        ),
-        appBarTheme: AppBarTheme(
-          color: AppColors.white,
-          elevation: 0,
-          titleTextStyle: GoogleFonts.quicksand(
-            color: AppColors.primary,
-            fontSize: 16.0,
-            fontWeight: FontWeight.w500,
+    return BlocProvider(
+      create: (context) => LoginBloc(AuthRemoteDatasouce()),
+      child: MaterialApp(
+        title: 'Flashlight Point of Sales App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+          useMaterial3: true,
+          textTheme: GoogleFonts.quicksandTextTheme(
+            Theme.of(context).textTheme,
           ),
-          iconTheme: const IconThemeData(
-            color: AppColors.primary,
+          appBarTheme: AppBarTheme(
+            color: AppColors.white,
+            elevation: 0,
+            titleTextStyle: GoogleFonts.quicksand(
+              color: AppColors.primary,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w500,
+            ),
+            iconTheme: const IconThemeData(
+              color: AppColors.primary,
+            ),
           ),
         ),
+        home: const SplashPage(),
       ),
-      home: const SplashPage(),
     );
   }
 }
