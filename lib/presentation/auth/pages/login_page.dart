dacff79e61d26f90183c1cc28e7a08d7ec6e0eee
muhammadshiva <shivamuhammad07@gmail.com>
2025-02-02 22:32:06 +0700

    improvement login page

--- lib/presentation/auth/pages/login_page.dart
+++ lib/presentation/auth/pages/login_page.dart
@@ -1,8 +1,9 @@
 import 'package:flashlight_pos_app/core/assets/assets.gen.dart';
 import 'package:flashlight_pos_app/core/components/buttons.dart';
-import 'package:flashlight_pos_app/core/components/custom_text_field.dart';
+import 'package:flashlight_pos_app/core/constant/styles/app_decoration.dart';
 import 'package:flashlight_pos_app/core/constant/styles/colors.dart';
 import 'package:flashlight_pos_app/core/constant/styles/fonts.dart';
+import 'package:flashlight_pos_app/core/utils/functions/form_validator.dart';
 import 'package:flashlight_pos_app/presentation/auth/bloc/login/login_bloc.dart';
 import 'package:flashlight_pos_app/presentation/auth/data/datasources/auth_local_datasource.dart';
 import 'package:flashlight_pos_app/presentation/customer/pages/customer_type_page.dart';
@@ -18,13 +19,29 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final usernameController = TextEditingController();
-  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  late final FocusNode usernameNode;
+  late final FocusNode passwordNode;
+  late final TextEditingController usernameCtrl;
+  late final TextEditingController passwordCtrl;
+  bool isShowPassword = false;
+  bool isPasswordFocus = false;
+
+  @override
+  void initState() {
+    usernameNode = FocusNode();
+    passwordNode = FocusNode();
+    usernameCtrl = TextEditingController();
+    passwordCtrl = TextEditingController();
+    super.initState();
+  }
 
   @override
   void dispose() {
-    usernameController.dispose();
-    passwordController.dispose();
+    usernameNode.dispose();
+    passwordNode.dispose();
+    usernameCtrl.dispose();
+    passwordCtrl.dispose();
     super.dispose();
   }
 
@@ -32,93 +49,162 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primary,
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-        children: [
-          80.verticalSpace,
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 130),
-            child: Image.asset(
-              Assets.images.logoFlashlight.path,
-              width: 100,
-              height: 100,
-            ),
-          ),
-          40.verticalSpace,
-          Center(
-            child: Text(
-              "Masuk untuk kasir",
-              style: AppTextStyle.barlow.copyWith(
-                fontSize: 18,
-                fontWeight: FontWeight.w400,
-                color: AppColors.lightWhite,
+      body: Form(
+        key: formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 200.w, vertical: 16.w),
+          children: [
+            80.verticalSpace,
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 130.w),
+              child: Image.asset(
+                Assets.images.logoFlashlight.path,
+                width: 100.w,
+                height: 100.w,
               ),
             ),
-          ),
-          20.verticalSpace,
-          CustomTextField(
-            controller: usernameController,
-            label: 'Username',
-            color: AppColors.lightWhite,
-          ),
-          20.verticalSpace,
-          CustomTextField(
-            controller: passwordController,
-            label: 'Password',
-            obscureText: true,
-            color: AppColors.lightWhite,
-          ),
-          40.verticalSpace,
-          BlocListener<LoginBloc, LoginState>(
-            listener: (context, state) {
-              state.maybeWhen(
-                orElse: () {},
-                success: (authResponseModel) async {
-                  AuthLocalDatasource().saveAuthData(authResponseModel);
-
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const CustomerTypePage(),
-                    ),
-                  );
-                },
-                error: (message) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(message),
-                      backgroundColor: Colors.red,
-                    ),
-                  );
-                },
-              );
-            },
-            child: BlocBuilder<LoginBloc, LoginState>(
-              builder: (context, state) {
-                return state.maybeWhen(
-                  orElse: () {
-                    return Button.filled(
-                      onPressed: () {
-                        context.read<LoginBloc>().add(
-                              LoginEvent.login(
-                                email: usernameController.text,
-                                password: passwordController.text,
-                              ),
-                            );
+            40.verticalSpace,
+            Center(
+              child: Text(
+                "Login Cashier",
+                style: AppTextStyle.poppins.copyWith(
+                  fontSize: 18.sp,
+                  color: AppColors.lightWhite,
+                ),
+              ),
+            ),
+            20.verticalSpace,
+            Text(
+              'Name',
+              style: AppTextStyle.poppins.copyWith(
+                color: AppColors.lightWhite,
+                fontSize: 12.sp,
+              ),
+            ),
+            12.verticalSpace,
+            TextFormField(
+              focusNode: usernameNode,
+              controller: usernameCtrl,
+              decoration: AppDecoration.inputDecoration.copyWith(
+                hintText: 'Input Name',
+              ),
+              style: AppTextStyle.poppins.copyWith(
+                color: AppColors.white,
+              ),
+              validator: FormValidator.validateName,
+            ),
+            20.verticalSpace,
+            Text(
+              'Password',
+              style: AppTextStyle.poppins.copyWith(
+                color: AppColors.lightWhite,
+                fontSize: 12.sp,
+              ),
+            ),
+            12.verticalSpace,
+            Focus(
+              onFocusChange: (hasFocus) {
+                setState(() {
+                  isPasswordFocus = hasFocus;
+                });
+              },
+              child: TextFormField(
+                focusNode: passwordNode,
+                controller: passwordCtrl,
+                obscureText: isShowPassword ? false : true,
+                decoration: AppDecoration.inputDecoration.copyWith(
+                  hintText: 'Input Password',
+                  suffixIcon: Transform.scale(
+                    scale: ScreenUtil().setSp(1),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (isShowPassword) {
+                            isShowPassword = false;
+                          } else {
+                            isShowPassword = true;
+                          }
+                        });
                       },
-                      label: 'Masuk',
+                      child: Align(
+                        widthFactor: 1.0,
+                        heightFactor: 1.0,
+                        child: !isShowPassword
+                            ? Icon(
+                                Icons.visibility_off,
+                                color: isPasswordFocus
+                                    ? AppColors.accentOrange
+                                    : Colors.grey,
+                              )
+                            : Icon(
+                                Icons.visibility,
+                                color: isPasswordFocus
+                                    ? AppColors.accentOrange
+                                    : Colors.grey,
+                              ),
+                      ),
+                    ),
+                  ),
+                ),
+                style: AppTextStyle.poppins.copyWith(
+                  color: AppColors.white,
+                ),
+                validator: FormValidator.validatePassword,
+              ),
+            ),
+            40.verticalSpace,
+            BlocListener<LoginBloc, LoginState>(
+              listener: (context, state) {
+                state.maybeWhen(
+                  orElse: () {},
+                  success: (authResponseModel) async {
+                    AuthLocalDatasource().saveAuthData(authResponseModel);
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const CustomerTypePage(),
+                      ),
                     );
                   },
-                  loading: () {
-                    return const Center(
-                      child: CircularProgressIndicator(),
+                  error: (message) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(message),
+                        backgroundColor: Colors.red,
+                      ),
                     );
                   },
                 );
               },
+              child: BlocBuilder<LoginBloc, LoginState>(
+                builder: (context, state) {
+                  return state.maybeWhen(
+                    orElse: () {
+                      return Button.filled(
+                        onPressed: () {
+                          context.read<LoginBloc>().add(
+                                LoginEvent.login(
+                                  email: usernameCtrl.text,
+                                  password: passwordCtrl.text,
+                                ),
+                              );
+                        },
+                        label: 'Masuk',
+                      );
+                    },
+                    loading: () {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    },
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
