247334e4dfd7b46c34400569bc54667969ca2498
muhammadshiva <shivamuhammad07@gmail.com>
2025-02-10 22:49:26 +0700

    remove unused code

--- lib/presentation/home/data/datasources/product/product_remote_datasource.dart
+++ lib/presentation/home/data/datasources/product/product_remote_datasource.dart
@@ -4,7 +4,6 @@ import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:flashlight_pos_app/core/constant/api/api_endpoint.dart';
 import 'package:flashlight_pos_app/core/utils/services/dio_service.dart';
-import 'package:flashlight_pos_app/presentation/auth/data/datasources/auth_local_datasource.dart';
 import 'package:flashlight_pos_app/presentation/home/data/models/product/response/product_model.dart';
 
 class ProductRemoteDatasource {
